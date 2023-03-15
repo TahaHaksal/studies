ae463ab9a382070874d9f2a307331dcc8f4428b4
Taha Haksal <91548254+TahaHaksal@users.noreply.github.com>
2023-03-15 11:01:31 +0300

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Exercise 1
+Route::prefix('/ex1')->group(function () {
+    Route::get('/', function () {
+        echo('Hello world');
+    });
+
+    Route::get('/required/{name}', function ($name) {
+        echo ('Hi ' . $name . '!');
+    });
+
+    Route::get('/optional/{name?}', function ($name = null) {
+        if ($name)
+            echo 'Hello ' . $name . '!';
+        else
+            echo 'Who are you?';
+    });
+
+    Route::get('/alphanumeric/{tag?}', function ($tag = null) {
+        if ($tag)
+            echo $tag . ' is alphanumeric!';
+        else
+            echo 'Please enter a tag';
+    })->where('tag', '[a-zA-Z0-9]+');
+
+    Route::match(['get', 'post'], '/match', function( Request $req) {
+        return 'Requested method is ' . $req->method();
+    });
+
+    Route::any('/any-method', function (Request $req) {
+        return 'Requested method is ' . $req->method();
+    });
+});
+//Exercise 1
