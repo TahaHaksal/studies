b02f99836af519408f30bf4761109b92ee6f0dba
Taha Haksal <91548254+TahaHaksal@users.noreply.github.com>
2023-03-16 14:19:29 +0300

    new file:   app/Http/Controllers/Ex6StubController.php 	new file:   stubs/cast.inbound.stub 	new file:   stubs/cast.stub 	new file:   stubs/console.stub 	new file:   stubs/controller.api.stub 	new file:   stubs/controller.invokable.stub 	new file:   stubs/controller.model.api.stub 	new file:   stubs/controller.model.stub 	new file:   stubs/controller.nested.api.stub 	new file:   stubs/controller.nested.singleton.api.stub 	new file:   stubs/controller.nested.singleton.stub 	new file:   stubs/controller.nested.stub 	new file:   stubs/controller.plain.stub 	new file:   stubs/controller.singleton.api.stub 	new file:   stubs/controller.singleton.stub 	new file:   stubs/controller.stub 	new file:   stubs/event.stub 	new file:   stubs/factory.stub 	new file:   stubs/job.queued.stub 	new file:   stubs/job.stub 	new file:   stubs/mail.stub 	new file:   stubs/markdown-mail.stub 	new file:   stubs/markdown-notification.stub 	new file:   stubs/middleware.stub 	new file:   stubs/migration.create.stub 	new file:   stubs/migration.stub 	new file:   stubs/migration.update.stub 	new file:   stubs/model.pivot.stub 	new file:   stubs/model.stub 	new file:   stubs/notification.stub 	new file:   stubs/observer.plain.stub 	new file:   stubs/observer.stub 	new file:   stubs/policy.plain.stub 	new file:   stubs/policy.stub 	new file:   stubs/provider.stub 	new file:   stubs/request.stub 	new file:   stubs/resource-collection.stub 	new file:   stubs/resource.stub 	new file:   stubs/rule.stub 	new file:   stubs/scope.stub 	new file:   stubs/seeder.stub 	new file:   stubs/test.stub 	new file:   stubs/test.unit.stub 	new file:   stubs/view-component.stub

---
+++ app/Http/Controllers/Ex6StubController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Ex6StubController extends Controller
+{
+    public function __construct(){
+        //
+    }
+
+    public function index(){
+        //
+    }
+
+    public function __destruct(){
+        //
+    }
+}
